f53071a8e00b1377f22f4e78b92415f98d63bd5f
luislima <luislimamn@gmail.com>
2025-04-25 19:29:06 -0300

    Versão 1.0.2025.04.25.1

--- lib/telas/lista_cripto_moedas.dart
+++ lib/telas/lista_cripto_moedas.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:lista_compras_mobile/configs/app_settings.dart';
 import 'package:lista_compras_mobile/models/moeda.dart';
 import 'package:lista_compras_mobile/repositories/favoritas_repository.dart';
 import 'package:lista_compras_mobile/repositories/moeda_repository.dart';
@@ -16,14 +17,48 @@ class ListaCriptoMoedas extends StatefulWidget {
 class _ListaCriptoMoedasState extends State<ListaCriptoMoedas> {
   Color color = Colors.blue;
   final tabela = MoedaRepository.tabela;
-  NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
+  //NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
+  late NumberFormat real;
+  late Map<String, dynamic> loc;
   List<Moeda> selecionadas = [];
   late FavoritasRepository favoritas;
+
+  readNumberFormat() {
+    loc = context.watch<AppSettings>().locale;
+    real = NumberFormat.currency(locale: loc['locale'], name: loc['name']);
+  }
+
+  changeLanguageButton() {
+    final texto = loc['locale'] == 'pt_BR' ? 'Dolar (\$)' : 'Real (R\$)';
+    final locale = loc['locale'] == 'pt_BR' ? 'en_US' : 'pt_BR';
+    final name = loc['locale'] == 'pt_BR' ? '\$' : 'R\$';
+
+    return PopupMenuButton(
+      icon: Icon(Icons.language),
+      itemBuilder: (context) => [
+        PopupMenuItem(
+            child: ListTile(
+              leading: Icon(Icons.swap_vert),
+              title: Text('Trocar para $texto'),
+              onTap: () {
+                context.read<AppSettings>().setLocale(locale, name);
+                Navigator.pop(context);
+              },
+            )),
+      ],
+    );
+  }
 
   appBarDinamica() {
     color = Theme.of(context).colorScheme.primary;
     if (selecionadas.isEmpty) {
-      return AppBar(backgroundColor: color, title: Text(widget.title));
+      return AppBar(
+        backgroundColor: color,
+        title: Text(widget.title),
+        actions: <Widget>[
+          changeLanguageButton(),
+        ],
+      );
     } else {
       return AppBar(
         leading: IconButton(
@@ -57,6 +92,7 @@ class _ListaCriptoMoedasState extends State<ListaCriptoMoedas> {
   Widget build(BuildContext context) {
     //favoritas = Provider.of<FavoritasRepository>(context);
     favoritas = context.watch<FavoritasRepository>();
+    readNumberFormat();
     return Scaffold(
       appBar: appBarDinamica(),
       body: ListView.separated(
