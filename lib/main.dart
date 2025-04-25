db7200919d15df1fb0f9a02979bc66f441d0a66b
luislima <luislimamn@gmail.com>
2025-04-25 16:32:27 -0300

    Versão 1.0.2025.04.25

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:lista_compras_mobile/configs/app_settings.dart';
+import 'package:lista_compras_mobile/configs/hive_config.dart';
 import 'package:lista_compras_mobile/repositories/favoritas_repository.dart';
 import 'package:lista_compras_mobile/telas/inoova_template.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await HiveConfig.start();
+
   runApp(
-    ChangeNotifierProvider(create: (context) => FavoritasRepository(),
+    MultiProvider(
+      providers: [
+        //ChangeNotifierProvider(create: (context) => ContaRepository()),
+        ChangeNotifierProvider(create: (context) => AppSettings()),
+        ChangeNotifierProvider(create: (context) => FavoritasRepository()),
+      ],
       child: const MyApp(),
     ),
   );
